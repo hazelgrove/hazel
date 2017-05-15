21366766707260becaa87787ede48718604645f5
Brandon Kase <brandon.kase@gmail.com>
2017-05-14 21:52:27 -0700

    Coq extraction script + simple ocaml translation

--- src/build.sh
+++ src/build.sh
@@ -1,8 +1,17 @@
+#!/bin/bash
+
+set -eou pipefail
+
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPTPATH"
+
 for f in `find . -name '*.ml'` ; \
 do ( \
 ocp-indent -i $f; \
 ); \
 done
+
+../core/extract-coq.sh
 
 ocamlbuild -use-ocamlfind \
   -pkgs js_of_ocaml,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,react,reactiveData,camomile \
