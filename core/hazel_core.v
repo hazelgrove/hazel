eaf70bd1a587178daed8e3cabdb89a387676634b
Brandon Kase <brandon.kase@gmail.com>
2017-06-21 15:10:28 +0200

    Syn-ana works and coq code fully extracts (#14)
    * Unsatisfyingly get syn-ana working with fuel
    
    * Extraction complete

--- core/hazel_core.v
+++ core/hazel_core.v
@@ -169,6 +169,27 @@ Module _Ctx <: CTX.
 End _Ctx.
 Module Ctx : CTX := _Ctx.
 
+Inductive Fuel : Type :=
+  | More : Fuel -> Fuel
+  | Kicked : Fuel.
+
+(* helpers that should be lifted out of this module probably *)
+Fixpoint map_option {A B : Type} (f : A -> B) (input : option A) : option B :=
+  match input with
+  | Some x => Some (f x)
+  | None => None
+  end.
+
+Fixpoint flatmap_option {A B : Type} (f : A -> option B) (input : option A) : option B :=
+  match input with
+  | Some x => f x
+  | None => None
+  end.
+
+Definition pipe_forward {A B : Type} (x : A) (f : A -> B) : B := f x.
+Notation "X |> F" := (pipe_forward X F)
+  (at level 40, left associativity) : core_scope.
+
 Module HExp.
   Inductive inj_side : Type :=
   | L : inj_side
@@ -193,142 +214,98 @@ Module HExp.
   | EmptyHole : t
   | NonEmptyHole : t -> t.
 
-  (* helpers that should be lifted out of this module probably *)
-  Fixpoint map_option {A B : Type} (f : A -> B) (input : option A) : option B :=
-    match input with
-    | Some x => Some (f x)
-    | None => None
-    end.
-
-  Fixpoint flatmap_option {A B : Type} (f : A -> option B) (input : option A) : option B :=
-    match input with
-    | Some x => f x
-    | None => None
-    end.
-
-  Definition pipe_forward {A B : Type} (x : A) (f : A -> B) : B := f x.
-  Notation "X |> F" := (pipe_forward X F)
-    (at level 40, left associativity) : core_scope.
-
-  Inductive Mode : Type := 
+  Inductive Mode : Type :=
   | Syn : Mode
   | Ana : HTyp.t -> Mode.
 
-  Inductive Result : Type := 
+  Inductive Result : Type :=
   | SynOK : HTyp.t -> Result
   | AnaOK : Result
   | IllTyped.
 
-  Fixpoint msynana (mode : Mode) (fuel : nat) (ctx : Ctx.t) (e : t) : Result := 
-  let syn := msynana Syn in 
-  match mode with 
-  | Syn => (
-    match e with
-    | Asc e' ty (* SAsc *) =>
-      let _ := msynana (Ana ty) (S(O)) ctx e' in SynOK ty
-    | _ => IllTyped
-    end
-  )
-  | Ana ty => (
-    match e with
-    | _ (* subsumption *) =>  
-		match fuel with 
-		| S(fuel') => (
-			let result := syn fuel' ctx e in 
-			match result with 
-			| SynOK(ty') => AnaOK
-			    (* if HTyp.consistent ty ty' then
-				  (Some tt)
-			     else None *)
-	        | _ => IllTyped
-	        end)
-		| _ => IllTyped
-	    end
-    end
-) end.
-
-  (* Fixpoint syn (fuel : nat) (ctx : Ctx.t) (e : t)
+  Fixpoint syn (fuel : Fuel) (ctx : Ctx.t) (e : t)
     : option HTyp.t :=
-    match e with
-    | Asc e' ty (* SAsc *) =>
-      let _ := ana fuel ctx e' ty in Some ty
-    | Var x (* SVar *) => Ctx.lookup ctx x
-    | Let x e1 e2 =>
-      (syn fuel ctx e1) |>
-          (map_option (fun ty1 => Ctx.extend ctx (x, ty1))) |>
-          (flatmap_option (fun ctx' => syn fuel ctx' e2))
-    | Ap e1 e2 (* SAp *) =>
-      let _ty1 := syn fuel ctx e1 in
-      match _ty1 with
-      | Some ty1 =>
-        match HTyp.matched_arrow ty1 with
-        | Some (ty1_left, ty1_right) =>
-            let _ := ana fuel ctx e2 ty1_left in Some ty1_right
-        | None => None
-        end
-      | None => None
-      end
-    | NumLit i (* SNum *) => Some HTyp.Num
-    | Plus e1 e2 (* 3e *) =>
-      let _ := ana fuel ctx e1 HTyp.Num in
-      let _ := ana fuel ctx e2 HTyp.Num in
-      Some HTyp.Num
-    | EmptyHole (* SHole *) => Some HTyp.Hole
-    | NonEmptyHole e' (* SNEHole *) =>
-      let _ := syn fuel ctx e' in
-      Some HTyp.Hole
-    | _ => None
-    end
-  with ana (fuel : nat) (ctx : Ctx.t) (e : t) (ty : HTyp.t)
-    : option unit :=
-    match e with
-    | Let x e1 e2 =>
-        syn fuel ctx e1 |>
-            map_option (fun ty1 => Ctx.extend ctx (x, ty1)) |>
-            flatmap_option (fun ctx' => ana fuel ctx' e2 ty)
-    | Lam x e' (* ALam *) =>
-      match HTyp.matched_arrow ty with
-      | Some (ty1, ty2) =>
-        let ctx' := Ctx.extend ctx (x, ty1) in
-        ana fuel ctx' e' ty2
-      | _ => None
-      end
-    | Inj side e' (* 21a *) =>
-      match HTyp.matched_sum ty with
-      | Some (ty1, ty2) => ana fuel ctx e' (pick_side side ty1 ty2)
-      | None => None
-      end
-    | Case e' (x, e1) (y, e2) (* 21b *) =>
-      let _e'_ty := syn fuel ctx e' in
-      match _e'_ty with
-      | Some e'_ty =>
-        match HTyp.matched_sum e'_ty with
-        | Some (ty1, ty2) =>
-          let ctx1 := Ctx.extend ctx (x, ty1) in
-          match (ana fuel ctx1 e1 ty) with
-          | Some _ =>
-            let ctx2 := Ctx.extend ctx (y, ty2) in
-            ana fuel ctx2 e2 ty
+    match fuel with
+    | More fuel =>
+      match e with
+      | Asc e' ty (* SAsc *) =>
+        let _ := ana fuel ctx e' ty in Some ty
+      | Var x (* SVar *) => Ctx.lookup ctx x
+      | Let x e1 e2 =>
+        (syn fuel ctx e1) |>
+            (map_option (fun ty1 => Ctx.extend ctx (x, ty1))) |>
+            (flatmap_option (fun ctx' => syn fuel ctx' e2))
+      | Ap e1 e2 (* SAp *) =>
+        let _ty1 := syn fuel ctx e1 in
+        match _ty1 with
+        | Some ty1 =>
+          match HTyp.matched_arrow ty1 with
+          | Some (ty1_left, ty1_right) =>
+              let _ := ana fuel ctx e2 ty1_left in Some ty1_right
           | None => None
           end
         | None => None
         end
-      | None => None
+      | NumLit i (* SNum *) => Some HTyp.Num
+      | Plus e1 e2 (* 3e *) =>
+        let _ := ana fuel ctx e1 HTyp.Num in
+        let _ := ana fuel ctx e2 HTyp.Num in
+        Some HTyp.Num
+      | EmptyHole (* SHole *) => Some HTyp.Hole
+      | NonEmptyHole e' (* SNEHole *) =>
+        let _ := syn fuel ctx e' in
+        Some HTyp.Hole
+      | _ => None
       end
-    | _ (* subsumption *) =>  
-		match fuel with 
-		| S(fuel') => (
-			let ty' := syn fuel' ctx e in 
-			match ty' with 
-			| Some ty' => None 
-			    (* if HTyp.consistent ty ty' then
-				  (Some tt)
-			     else None *)
-	        | None => None
-	        end)
-		| _ => None
-	    end
-    end. *)
+    | Kicked => None
+    end
+  with ana (fuel : Fuel) (ctx : Ctx.t) (e : t) (ty : HTyp.t)
+    : option unit :=
+    match fuel with
+    | More fuel =>
+      match e with
+      | Let x e1 e2 =>
+          syn fuel ctx e1 |>
+              map_option (fun ty1 => Ctx.extend ctx (x, ty1)) |>
+              flatmap_option (fun ctx' => ana fuel ctx' e2 ty)
+      | Lam x e' (* ALam *) =>
+        match HTyp.matched_arrow ty with
+        | Some (ty1, ty2) =>
+          let ctx' := Ctx.extend ctx (x, ty1) in
+          ana fuel ctx' e' ty2
+        | _ => None
+        end
+      | Inj side e' (* 21a *) =>
+        match HTyp.matched_sum ty with
+        | Some (ty1, ty2) => ana fuel ctx e' (pick_side side ty1 ty2)
+        | None => None
+        end
+      | Case e' (x, e1) (y, e2) (* 21b *) =>
+        let _e'_ty := syn fuel ctx e' in
+        match _e'_ty with
+        | Some e'_ty =>
+          match HTyp.matched_sum e'_ty with
+          | Some (ty1, ty2) =>
+            let ctx1 := Ctx.extend ctx (x, ty1) in
+            match (ana fuel ctx1 e1 ty) with
+            | Some _ =>
+              let ctx2 := Ctx.extend ctx (y, ty2) in
+              ana fuel ctx2 e2 ty
+            | None => None
+            end
+          | None => None
+          end
+        | None => None
+        end
+      | _ (* subsumption *) =>
+        match syn fuel ctx e with
+        | Some ty' =>
+            if HTyp.consistent ty ty' then (Some tt) else None
+        | None => None
+        end
+      end
+    | Kicked => None
+    end.
 
   Fixpoint complete (e : t) : bool :=
     match e with
@@ -429,124 +406,107 @@ Module Action.
   | Construct : shape -> t
   | Finish : t.
 
-  (*Fixpoint performTyp (a : t) (zty : ZTyp.t) : M [ InvalidAction ] ZTyp.t :=*)
-    (*match (a, zty) with*)
-    (*| (Move (Child 1), ZTyp.CursorT (HTyp.Arrow ty1 ty2)) =>*)
-      (*ret (ZTyp.LeftArrow (ZTyp.CursorT ty1) ty2)*)
-    (*| (Move (Child 2), ZTyp.CursorT (HTyp.Arrow ty1 ty2)) =>*)
-      (*ret (ZTyp.RightArrow ty1 (ZTyp.CursorT ty2))*)
-    (*| (Move Parent, ZTyp.LeftArrow (ZTyp.CursorT ty1) ty2) =>*)
-      (*ret (ZTyp.CursorT (HTyp.Arrow ty1 ty2))*)
-    (*| (Move Parent, ZTyp.RightArrow ty1 (ZTyp.CursorT ty2)) =>*)
-      (*ret (ZTyp.CursorT (HTyp.Arrow ty1 ty2))*)
-    (*| (Move (Child 1), ZTyp.CursorT (HTyp.Sum ty1 ty2)) =>*)
-      (*ret (ZTyp.LeftSum (ZTyp.CursorT ty1) ty2)*)
-    (*| (Move (Child 2), ZTyp.CursorT (HTyp.Sum ty1 ty2)) =>*)
-      (*ret (ZTyp.RightSum ty1 (ZTyp.CursorT ty2))*)
-    (*| (Move Parent, ZTyp.LeftSum (ZTyp.CursorT ty1) ty2) =>*)
-      (*ret (ZTyp.CursorT (HTyp.Sum ty1 ty2))*)
-    (*| (Move Parent, ZTyp.RightSum ty1 (ZTyp.CursorT ty2)) =>*)
-      (*ret (ZTyp.CursorT (HTyp.Sum ty1 ty2))*)
-    (*| (Del, ZTyp.CursorT ty) => ret (ZTyp.CursorT HTyp.Hole)*)
-    (*| (Construct SArrow, ZTyp.CursorT ty) =>*)
-      (*ret (ZTyp.RightArrow ty (ZTyp.CursorT HTyp.Hole))*)
-    (*| (Construct SNum, ZTyp.CursorT HTyp.Hole) => ret (ZTyp.CursorT HTyp.Num)*)
-    (*| (Construct SSum, ZTyp.CursorT ty) =>*)
-      (*ret (ZTyp.RightSum ty (ZTyp.CursorT HTyp.Hole))*)
-    (*| (_, ZTyp.LeftArrow zty1 ty2) =>*)
-      (*let! zty1' := performTyp a zty1 in*)
-      (*ret (ZTyp.LeftArrow zty1' ty2)*)
-    (*| (_, ZTyp.RightArrow ty1 zty2) =>*)
-      (*let! zty2' := performTyp a zty2 in*)
-      (*ret (ZTyp.RightArrow ty1 zty2')*)
-    (*| (_, ZTyp.LeftSum zty1 ty2) =>*)
-      (*let! zty1' := performTyp a zty1 in*)
-      (*ret (ZTyp.LeftSum zty1' ty2)*)
-    (*| (_, ZTyp.RightSum ty1 zty2) =>*)
-      (*let! zty2' := performTyp a zty2 in*)
-      (*ret (ZTyp.RightSum ty1 zty2')*)
-    (*| _ => raise_InvalidAction tt*)
-    (*end.*)
+  Fixpoint performTyp (a : t) (zty : ZTyp.t) : option ZTyp.t :=
+    match (a, zty) with
+    | (Move (Child 1), ZTyp.CursorT (HTyp.Arrow ty1 ty2)) =>
+      Some (ZTyp.LeftArrow (ZTyp.CursorT ty1) ty2)
+    | (Move (Child 2), ZTyp.CursorT (HTyp.Arrow ty1 ty2)) =>
+      Some (ZTyp.RightArrow ty1 (ZTyp.CursorT ty2))
+    | (Move Parent, ZTyp.LeftArrow (ZTyp.CursorT ty1) ty2) =>
+      Some (ZTyp.CursorT (HTyp.Arrow ty1 ty2))
+    | (Move Parent, ZTyp.RightArrow ty1 (ZTyp.CursorT ty2)) =>
+      Some (ZTyp.CursorT (HTyp.Arrow ty1 ty2))
+    | (Move (Child 1), ZTyp.CursorT (HTyp.Sum ty1 ty2)) =>
+      Some (ZTyp.LeftSum (ZTyp.CursorT ty1) ty2)
+    | (Move (Child 2), ZTyp.CursorT (HTyp.Sum ty1 ty2)) =>
+      Some (ZTyp.RightSum ty1 (ZTyp.CursorT ty2))
+    | (Move Parent, ZTyp.LeftSum (ZTyp.CursorT ty1) ty2) =>
+      Some (ZTyp.CursorT (HTyp.Sum ty1 ty2))
+    | (Move Parent, ZTyp.RightSum ty1 (ZTyp.CursorT ty2)) =>
+      Some (ZTyp.CursorT (HTyp.Sum ty1 ty2))
+    | (Del, ZTyp.CursorT ty) => Some (ZTyp.CursorT HTyp.Hole)
+    | (Construct SArrow, ZTyp.CursorT ty) =>
+      Some (ZTyp.RightArrow ty (ZTyp.CursorT HTyp.Hole))
+    | (Construct SNum, ZTyp.CursorT HTyp.Hole) => Some (ZTyp.CursorT HTyp.Num)
+    | (Construct SSum, ZTyp.CursorT ty) =>
+      Some (ZTyp.RightSum ty (ZTyp.CursorT HTyp.Hole))
+    | (_, ZTyp.LeftArrow zty1 ty2) =>
+        (performTyp a zty1) |>
+          flatmap_option(fun zty1' => Some (ZTyp.LeftArrow zty1' ty2))
+    | (_, ZTyp.RightArrow ty1 zty2) => (performTyp a zty2) |>
+          flatmap_option(fun zty2' => Some (ZTyp.RightArrow ty1 zty2'))
+    | (_, ZTyp.LeftSum zty1 ty2) => (performTyp a zty1) |>
+          flatmap_option(fun zty1' => Some (ZTyp.LeftSum zty1' ty2))
+    | (_, ZTyp.RightSum ty1 zty2) => (performTyp a zty2) |>
+          flatmap_option(fun zty2' => Some (ZTyp.RightSum ty1 zty2'))
+    | _ => None
+    end.
 
-  (*Fixpoint performEMove (action : t) (ze : ZExp.t)*)
-    (*: M [ InvalidAction ] ZExp.t :=*)
-    (*match action with*)
-    (*| Move direction =>*)
-      (*match (direction, ze) with*)
-      (*| (Child 1, ZExp.CursorE (HExp.Asc e ty)) =>*)
-        (*ret (ZExp.LeftAsc (ZExp.CursorE e) ty)*)
-      (*| (Child 2, ZExp.CursorE (HExp.Asc e ty)) =>*)
-        (*ret (ZExp.RightAsc e (ZTyp.CursorT ty))*)
-      (*| (Parent, ZExp.LeftAsc (ZExp.CursorE e) ty) =>*)
-        (*ret (ZExp.CursorE (HExp.Asc e ty))*)
-      (*| (Parent, ZExp.RightAsc e (ZTyp.CursorT ty)) =>*)
-        (*ret (ZExp.CursorE (HExp.Asc e ty))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Let x e e')) =>*)
-        (*ret (ZExp.LetZ1 x (ZExp.CursorE e) e')*)
-      (*| (Child 2, ZExp.CursorE (HExp.Let x e e')) =>*)
-        (*ret (ZExp.LetZ2 x e (ZExp.CursorE e'))*)
-      (*| (Parent, ZExp.LetZ1 x (ZExp.CursorE e) e') =>*)
-        (*ret (ZExp.CursorE (HExp.Let x e e'))*)
-      (*| (Parent, ZExp.LetZ2 x e (ZExp.CursorE e')) =>*)
-        (*ret (ZExp.CursorE (HExp.Let x e e'))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Lam x e)) =>*)
-        (*ret (ZExp.LamZ x (ZExp.CursorE e))*)
-      (*| (Parent, ZExp.LamZ x (ZExp.CursorE e)) =>*)
-        (*ret (ZExp.CursorE (HExp.Lam x e))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Ap e1 e2)) =>*)
-        (*ret (ZExp.LeftAp (ZExp.CursorE e1) e2)*)
-      (*| (Child 2, ZExp.CursorE (HExp.Ap e1 e2)) =>*)
-        (*ret (ZExp.RightAp e1 (ZExp.CursorE e2))*)
-      (*| (Parent, ZExp.LeftAp (ZExp.CursorE e1) e2) =>*)
-        (*ret (ZExp.CursorE (HExp.Ap e1 e2))*)
-      (*| (Parent, ZExp.RightAp e1 (ZExp.CursorE e2)) =>*)
-        (*ret (ZExp.CursorE (HExp.Ap e1 e2))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Plus e1 e2)) =>*)
-        (*ret (ZExp.LeftPlus (ZExp.CursorE e1) e2)*)
-      (*| (Child 2, ZExp.CursorE (HExp.Plus e1 e2)) =>*)
-        (*ret (ZExp.RightPlus e1 (ZExp.CursorE e2))*)
-      (*| (Parent, ZExp.LeftPlus (ZExp.CursorE e1) e2) =>*)
-        (*ret (ZExp.CursorE (HExp.Plus e1 e2))*)
-      (*| (Parent, ZExp.RightPlus e1 (ZExp.CursorE e2)) =>*)
-        (*ret (ZExp.CursorE (HExp.Plus e1 e2))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Inj side e)) =>*)
-        (*ret (ZExp.InjZ side (ZExp.CursorE e))*)
-      (*| (Parent, ZExp.InjZ side (ZExp.CursorE e)) =>*)
-        (*ret (ZExp.CursorE (HExp.Inj side e))*)
-      (*| (Child 1, ZExp.CursorE (HExp.Case e branch1 branch2)) =>*)
-        (*ret (ZExp.CaseZ1 (ZExp.CursorE e) branch1 branch2)*)
-      (*| (Child 2, ZExp.CursorE (HExp.Case e (x, e1) branch2)) =>*)
-        (*ret (ZExp.CaseZ2 e (x, (ZExp.CursorE e1)) branch2)*)
-      (*| (Child 3, ZExp.CursorE (HExp.Case e branch1 (y, e2))) =>*)
-        (*ret (ZExp.CaseZ3 e branch1 (y, (ZExp.CursorE e2)))*)
-      (*| (Parent, ZExp.CaseZ1 (ZExp.CursorE e) branch1 branch2) =>*)
-        (*ret (ZExp.CursorE (HExp.Case e branch1 branch2))*)
-      (*| (Parent, ZExp.CaseZ2 e (x, ZExp.CursorE e1) branch2) =>*)
-        (*ret (ZExp.CursorE (HExp.Case e (x, e1) branch2))*)
-      (*| (Parent, ZExp.CaseZ3 e branch1 (y, ZExp.CursorE e2)) =>*)
-        (*ret (ZExp.CursorE (HExp.Case e branch1 (y, e2)))*)
-      (*| (Child 1, ZExp.CursorE (HExp.NonEmptyHole e)) =>*)
-        (*ret (ZExp.NonEmptyHoleZ (ZExp.CursorE e))*)
-      (*| (Parent, ZExp.NonEmptyHoleZ (ZExp.CursorE e)) =>*)
-        (*ret (ZExp.CursorE (HExp.NonEmptyHole e))*)
-      (*| _ => raise_InvalidAction tt*)
-      (*end*)
-    (*| _ => raise_InvalidAction tt*)
-    (*end.*)
-
-  (*Definition hsyn (ctx : Ctx.t) (e : HExp.t)*)
-    (*: M [ Counter; NonTermination; InvalidAction ] HTyp.t :=*)
-    (*match HExp.syn ctx e with*)
-    (*| Some x => ret x*)
-    (*| None => lift [_;_;_] "001" (raise_InvalidAction tt)*)
-    (*end.*)
-
-  (*Definition hana (ctx : Ctx.t) (e : HExp.t) (ty : HTyp.t)*)
-    (*: M [ Counter; NonTermination; InvalidAction ] unit :=*)
-    (*match HExp.ana ctx e ty with*)
-    (*| Some x => ret x*)
-    (*| None => lift [_;_;_] "001" (raise_InvalidAction tt)*)
-    (*end.*)
+  Fixpoint performEMove (action : t) (ze : ZExp.t)
+    : option ZExp.t :=
+    match action with
+    | Move direction =>
+      match (direction, ze) with
+      | (Child 1, ZExp.CursorE (HExp.Asc e ty)) =>
+        Some (ZExp.LeftAsc (ZExp.CursorE e) ty)
+      | (Child 2, ZExp.CursorE (HExp.Asc e ty)) =>
+        Some (ZExp.RightAsc e (ZTyp.CursorT ty))
+      | (Parent, ZExp.LeftAsc (ZExp.CursorE e) ty) =>
+        Some (ZExp.CursorE (HExp.Asc e ty))
+      | (Parent, ZExp.RightAsc e (ZTyp.CursorT ty)) =>
+        Some (ZExp.CursorE (HExp.Asc e ty))
+      | (Child 1, ZExp.CursorE (HExp.Let x e e')) =>
+        Some (ZExp.LetZ1 x (ZExp.CursorE e) e')
+      | (Child 2, ZExp.CursorE (HExp.Let x e e')) =>
+        Some (ZExp.LetZ2 x e (ZExp.CursorE e'))
+      | (Parent, ZExp.LetZ1 x (ZExp.CursorE e) e') =>
+        Some (ZExp.CursorE (HExp.Let x e e'))
+      | (Parent, ZExp.LetZ2 x e (ZExp.CursorE e')) =>
+        Some (ZExp.CursorE (HExp.Let x e e'))
+      | (Child 1, ZExp.CursorE (HExp.Lam x e)) =>
+        Some (ZExp.LamZ x (ZExp.CursorE e))
+      | (Parent, ZExp.LamZ x (ZExp.CursorE e)) =>
+        Some (ZExp.CursorE (HExp.Lam x e))
+      | (Child 1, ZExp.CursorE (HExp.Ap e1 e2)) =>
+        Some (ZExp.LeftAp (ZExp.CursorE e1) e2)
+      | (Child 2, ZExp.CursorE (HExp.Ap e1 e2)) =>
+        Some (ZExp.RightAp e1 (ZExp.CursorE e2))
+      | (Parent, ZExp.LeftAp (ZExp.CursorE e1) e2) =>
+        Some (ZExp.CursorE (HExp.Ap e1 e2))
+      | (Parent, ZExp.RightAp e1 (ZExp.CursorE e2)) =>
+        Some (ZExp.CursorE (HExp.Ap e1 e2))
+      | (Child 1, ZExp.CursorE (HExp.Plus e1 e2)) =>
+        Some (ZExp.LeftPlus (ZExp.CursorE e1) e2)
+      | (Child 2, ZExp.CursorE (HExp.Plus e1 e2)) =>
+        Some (ZExp.RightPlus e1 (ZExp.CursorE e2))
+      | (Parent, ZExp.LeftPlus (ZExp.CursorE e1) e2) =>
+        Some (ZExp.CursorE (HExp.Plus e1 e2))
+      | (Parent, ZExp.RightPlus e1 (ZExp.CursorE e2)) =>
+        Some (ZExp.CursorE (HExp.Plus e1 e2))
+      | (Child 1, ZExp.CursorE (HExp.Inj side e)) =>
+        Some (ZExp.InjZ side (ZExp.CursorE e))
+      | (Parent, ZExp.InjZ side (ZExp.CursorE e)) =>
+        Some (ZExp.CursorE (HExp.Inj side e))
+      | (Child 1, ZExp.CursorE (HExp.Case e branch1 branch2)) =>
+        Some (ZExp.CaseZ1 (ZExp.CursorE e) branch1 branch2)
+      | (Child 2, ZExp.CursorE (HExp.Case e (x, e1) branch2)) =>
+        Some (ZExp.CaseZ2 e (x, (ZExp.CursorE e1)) branch2)
+      | (Child 3, ZExp.CursorE (HExp.Case e branch1 (y, e2))) =>
+        Some (ZExp.CaseZ3 e branch1 (y, (ZExp.CursorE e2)))
+      | (Parent, ZExp.CaseZ1 (ZExp.CursorE e) branch1 branch2) =>
+        Some (ZExp.CursorE (HExp.Case e branch1 branch2))
+      | (Parent, ZExp.CaseZ2 e (x, ZExp.CursorE e1) branch2) =>
+        Some (ZExp.CursorE (HExp.Case e (x, e1) branch2))
+      | (Parent, ZExp.CaseZ3 e branch1 (y, ZExp.CursorE e2)) =>
+        Some (ZExp.CursorE (HExp.Case e branch1 (y, e2)))
+      | (Child 1, ZExp.CursorE (HExp.NonEmptyHole e)) =>
+        Some (ZExp.NonEmptyHoleZ (ZExp.CursorE e))
+      | (Parent, ZExp.NonEmptyHoleZ (ZExp.CursorE e)) =>
+        Some (ZExp.CursorE (HExp.NonEmptyHole e))
+      | _ => None
+      end
+    | _ => None
+    end.
 
 End Action.
 
@@ -559,9 +519,7 @@ Extract Inductive prod => "(*)" ["(,)"].
 Extract Inductive list => "list" [ "[]" "(::)" ].
 Extract Inductive nat => int [ "0" "succ" ]
        "(fun fO fS n -> if n=0 then fO () else fS (n-1))".
-(* TODO: Only extract "Action" when it is properly using all of the other modules *)
-Extraction HTyp.
-Extraction HExp.
-Extraction ZTyp.
-Extraction ZExp.
+Extract Inductive Fuel => "unit" [ "()" "()" ] "(fun fMore _ fKicked -> fMore ())".
+(*Extract Inlined Constant pipe_forward => "(|>)".*)
+
 Extraction Action.
