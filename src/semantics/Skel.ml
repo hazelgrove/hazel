d5ed7495d5e6153d1876cedbbf45f3d48638fac0
David Moon <dmoon1221@gmail.com>
2019-02-03 21:16:47 -0700

    Add Skel

---
+++ src/semantics/Skel.ml
@@ -0,0 +1,20 @@
+module Skel =
+ struct
+  type 'op t =
+  | Placeholder of int
+  | BinOp of err_status * 'op * 'op t * 'op t
+
+  let rec leftmost_op = function
+  | Placeholder _ -> None
+  | BinOp (_, op, skel1, _) ->
+    (match leftmost_op skel1 with
+     | Some op1 -> Some op1
+     | None -> Some op)
+
+  let rec rightmost_op = function
+  | Placeholder _ -> None
+  | BinOp (_, op, _, skel2) ->
+    (match rightmost_op skel2 with
+     | Some op1 -> Some op1
+     | None -> Some op)
+ end
