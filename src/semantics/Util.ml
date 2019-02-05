22d4ccd546667d738257bc1790fc686c7b459783
Cyrus Omar <cyology@gmail.com>
2019-02-04 18:33:49 -0600

    fixes to Util.ml

--- src/semantics/Util.ml
+++ src/semantics/Util.ml
@@ -1,40 +1,47 @@
 module Util =
  struct
+   type nat = int
 
   (* Section ListUtil *)
 
   let rec update_nth n xs f =
-    match (n, xs) with
+    begin match (n, xs) with
     | (_, []) -> []
     | (0, x::xs) -> (f x)::xs
     | (n, x::xs) -> x::(update_nth (n-1) xs f)
+    end
 
   let rec _findmapi i xs f =
-    match xs with
+    begin match xs with
     | [] -> None
     | x::xs ->
-      match f i x with
+      begin match f i x with
       | Some b -> Some b
-      | None -> _findmapi (i+1) xs f)
+      | None -> _findmapi (i+1) xs f
+      end
+    end
 
   let findmapi xs f = _findmapi 0 xs f
 
   let rec zip_eq xs ys =
-    match (xs, ys) with
-    | ([], []) => Some []
-    | (x::xs, y::ys) =>
-      match zip_eq xs ys with
-      | None => None
-      | Some tail => Some ((x,y)::tail)
-    | (_::_, []) => None
-    | ([], _::_) => None
+    begin match (xs, ys) with
+    | ([], []) -> Some []
+    | (x::xs, y::ys) ->
+      begin match zip_eq xs ys with
+      | None -> None
+      | Some tail -> Some ((x,y)::tail)
+      end
+    | (_::_, []) -> None
+    | ([], _::_) -> None
+    end
 
   let rec unzip xs =
-    match xs with
+    begin match xs with
     | [] -> [],[]
     | (x,y)::xys ->
       let xs,ys = unzip xys in
       (x::xs),(y::ys)
+    end
 
   (* End ListUtil *)
 
@@ -60,49 +67,54 @@ module Util =
     let extend delta x = x::delta
 
     let rec drop delta n =
-      match delta with
+      begin match delta with
       | [] -> None
       | (y,a)::delta' ->
-        if eqb n y then Some (delta',a) else drop delta' n
+        if n == y then Some (delta',a) else drop delta' n
+      end
 
     let union = app
 
     let rec lookup delta x =
-      match delta with
+      begin match delta with
       | [] -> None
       | (y,a)::delta' ->
         if eqb x y then Some a else lookup delta' x
+      end
 
     let rec insert_or_update delta x =
       let u,a = x in
-      match delta with
+      begin match delta with
       | [] -> x::delta
       | (u',a')::delta' ->
         if eqb u u'
         then (u',a)::delta'
         else (u',a')::(insert_or_update delta' x)
+      end
 
     let rec insert_or_map delta u a0 f =
-      match delta with
+      begin match delta with
       | [] -> let a0 = a0 () in a0,((u,a0)::delta)
       | (u'a)::delta' ->
         if eqb u u'
         then let a' = f a in a',((u',a')::delta')
         else let a',delta'' = insert_or_map delta' u a0 f in
              a',((u',a)::delta'')
+      end
 
     let rec map f = function
     | [] -> []
     | (u,a)::delta' -> (u,(f a))::(map f delta')
 
     let rec update_with f u delta u_nil =
-      match delta with
+      begin match delta with
       | [] -> u_nil,delta
       | (u',a)::delta' ->
         if eqb u u'
         then let a' = f a in a',((u',a')::delta')
         else let a',delta'' = update_with f u delta' u_nil in
              a',((u',a)::delta'')
+      end
 
     let length = length
 
