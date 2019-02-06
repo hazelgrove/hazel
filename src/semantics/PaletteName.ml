85967a020b546135919701dbd4ea3d6d6c11ca79
Cyrus Omar <cyology@gmail.com>
2019-02-06 00:20:00 -0600

    changed Var/PaletteName validity functions to use a regex instead of old code

--- src/semantics/PaletteName.ml
+++ src/semantics/PaletteName.ml
@@ -1,10 +1,11 @@
 type t = string
 let eq (x : t) (y : t) = x == y
-let rec _is_valid_internal = Var.is_valid
-let is_valid s =
-  (* should be equivalent to the OCaml rules: "[_a-z][_a-zA-Z0-9']*" *)
-  if s = ""
-  then false
-  else (Util.char_eq_b first_char '$') && _is_valid_internal rest
+let _is_valid_internal = Var.is_valid
+let valid_regex = Js_of_ocaml.Regexp.regexp "^\\$[_a-zA-Z][_a-zA-Z0-9']*$"
+let is_valid s = 
+  begin match Js_of_ocaml.Regexp.string_match valid_regex s 0 with 
+  | Some _ -> true
+  | None -> false
+  end
 let check_valid s result =
   if is_valid s then result else None
