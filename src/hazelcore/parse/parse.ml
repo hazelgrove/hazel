7979af2d91cc4f69c1756b7046bc73d85c150007
Nicholas Wilson <njwilsondc@gmail.com>
2020-12-13 18:18:10 -0500

    Moved parser into hazelcore

--- src/hazelcore/parse/parse.ml
+++ src/hazelcore/parse/parse.ml
@@ -18,7 +18,9 @@ let rec parse lexbuf c =
   | I.HandlingError _ ->
       let startp = lexbuf.lex_start_p in
       let line = startp.pos_lnum in
-      let col = startp.pos_cnum + 1 in
-      raise (SyntaxError (Some (line, col), "Syntax error: "))
+      let col = startp.pos_cnum - startp.pos_bol + 1 in
+      let test = Lexing.lexeme lexbuf in
+      let _ = print_endline test in
+      raise (SyntaxError (Some (line, col), test))
   | I.Accepted v -> v
   | I.Rejected -> raise (SyntaxError (None, "Rejected"))
