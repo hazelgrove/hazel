fe2c6f6f03e88c628276c93cbf00efb8c410ce8a
Nicholas Wilson <njwilsondc@gmail.com>
2021-11-11 23:31:03 -0500

    Formatting

--- src/hazeltext/parsing.ml
+++ src/hazeltext/parsing.ml
@@ -12,9 +12,9 @@ let rec parse lexbuf c =
       let endp = lexbuf.lex_curr_p in
       let checkpoint = I.offer c (token, startp, endp) in
       parse lexbuf checkpoint
-  | I.Shifting _
-  | I.AboutToReduce _ ->
-      let checkpoint = I.resume c in parse lexbuf checkpoint
+  | I.Shifting _ | I.AboutToReduce _ ->
+      let checkpoint = I.resume c in
+      parse lexbuf checkpoint
   | I.HandlingError _ ->
       let startp = lexbuf.lex_start_p in
       let line = startp.pos_lnum in
@@ -22,8 +22,8 @@ let rec parse lexbuf c =
       let lexeme =
         match startp.pos_cnum < lexbuf.lex_buffer_len with
         | true ->
-          let c = Bytes.get (lexbuf.lex_buffer) startp.pos_cnum in
-          Some (String.make 1 c)
+            let c = Bytes.get lexbuf.lex_buffer startp.pos_cnum in
+            Some (String.make 1 c)
         | false -> None
       in
       raise (SyntaxError (Some (line, col), lexeme))
@@ -31,21 +31,15 @@ let rec parse lexbuf c =
   | I.Rejected -> raise (SyntaxError (None, Some "Rejected"))
 
 let ast_of_lexbuf l =
-  try
-    Some ((parse l (Parse.Incremental.main l.lex_curr_p))), None
-  with
-  | SyntaxError((Some(line, col), tok)) ->
+  try (Some (parse l (Parse.Incremental.main l.lex_curr_p)), None) with
+  | SyntaxError (Some (line, col), tok) ->
       let tok_string =
         match tok with
-        | Some (c) -> Printf.sprintf "Token: %s" c
-        | None -> Printf.sprintf("End of File")
+        | Some c -> Printf.sprintf "Token: %s" c
+        | None -> Printf.sprintf "End of File"
       in
       let err_string =
-        Printf.sprintf
-          "ERROR on line %d, column %d. %s"
-          line
-          col
-          tok_string
+        Printf.sprintf "ERROR on line %d, column %d. %s" line col tok_string
       in
-      None, Some err_string
-  | SyntaxError((None, _)) -> (None, Some "Unknown Error")
+      (None, Some err_string)
+  | SyntaxError (None, _) -> (None, Some "Unknown Error")
--- src/hazeltext/test/test.ml
+++ src/hazeltext/test/test.ml
@@ -1,79 +1,82 @@
 module Parsing = Hazeltext.Parsing
 module Parse = Hazeltext.Parse
 module Print = Hazeltext.Print
-module UHDoc_Exp = UHDoc_Exp.Make(Memo.DummyMemo)
+module UHDoc_Exp = UHDoc_Exp.Make (Memo.DummyMemo)
 
-let parse text: UHExp.block option =
+let parse text : UHExp.block option =
   let lexbuf = Lexing.from_string text in
-  let (ast, _) = Parsing.ast_of_lexbuf lexbuf in
+  let ast, _ = Parsing.ast_of_lexbuf lexbuf in
   ast
 
-let test_parse text: bool =
- (*Get the first AST*)
+let test_parse text : bool =
+  (*Get the first AST*)
   let ast_a = parse text in
   match ast_a with
-  | Some(ast) -> (
-    let doc = Lazy.force UHDoc_Exp.mk ~memoize:true ~enforce_inline:false ast in
-    let lay = Pretty.LayoutOfDoc.layout_of_doc ~width:100 ~pos:0 doc in
-    try
-      let l = match lay with
-      | Some l -> l
-      | None -> raise Print.NoLayout
+  | Some ast -> (
+      let doc =
+        Lazy.force UHDoc_Exp.mk ~memoize:true ~enforce_inline:false ast
       in
-      let printed_text = Print.string_of_layout l in
-      (*Get the seconds AST*)
-      let ast_b = parse printed_text in
-      ast_a = ast_b
-    with
-    | Print.NoLayout -> false
-  )
+      let lay = Pretty.LayoutOfDoc.layout_of_doc ~width:100 ~pos:0 doc in
+      try
+        let l = match lay with Some l -> l | None -> raise Print.NoLayout in
+        let printed_text = Print.string_of_layout l in
+        (*Get the seconds AST*)
+        let ast_b = parse printed_text in
+        ast_a = ast_b
+      with Print.NoLayout -> false)
   | None -> false
 
-let%test "basic types" =
-  test_parse "1; two; 3.0; true; false"
+let%test "basic types" = test_parse "1; two; 3.0; true; false"
 
-let%test "let basic" =
-  test_parse "let a = 1 in a"
+let%test "let basic" = test_parse "let a = 1 in a"
 
-let%test "let type annotation" =
-  test_parse "let a : Int = 1 in a"
+let%test "let type annotation" = test_parse "let a : Int = 1 in a"
 
-let%test "basic lambda" =
-  test_parse "\\f.{f}"
+let%test "basic lambda" = test_parse "\\f.{f}"
 
-let%test "multiline" =
-  test_parse "let a =\n 1\n in\n a"
+let%test "multiline" = test_parse "let a =\n 1\n in\n a"
 
-let%test "comment" =
-  test_parse "#Comment\n 3"
+let%test "comment" = test_parse "#Comment\n 3"
 
 (* Currently, the final line must be an Exp line *)
-let%test "bad comment" =
-  test_parse "#Comment \n 3; #Comment"
-  = false
+let%test "bad comment" = test_parse "#Comment \n 3; #Comment" = false
 
 let%test "mult" =
-  test_parse "
-    let mult : [Int] -> Int =
-      \\list.{
-        case list
-        | hd::[] => hd
-        | hd::md::[] => hd * md
-        | hd::tl => (mult tl)
-        end
-      }
-    in
-    mult (4::3::[])"
+  test_parse
+    "\n\
+    \    let mult : [Int] -> Int =\n\
+    \      \\list.{\n\
+    \        case list\n\
+    \        | hd::[] => hd\n\
+    \        | hd::md::[] => hd * md\n\
+    \        | hd::tl => (mult tl)\n\
+    \        end\n\
+    \      }\n\
+    \    in\n\
+    \    mult (4::3::[])"
 
 let%test "map" =
-  test_parse "
-  let map : (Int -> Int) -> [Int] -> [Int] =
-      \\f.{
-        \\xs.{
-          case xs
-          | [] => []
-          | y::ys => (f y)::(map f ys)
-          end
-        }
-      }
-  in map"
+  test_parse
+    "\n\
+    \  let map : (Int -> Int) -> [Int] -> [Int] =\n\
+    \      \\f.{\n\
+    \        \\xs.{\n\
+    \          case xs\n\
+    \          | [] => []\n\
+    \          | y::ys => (f y)::(map f ys)\n\
+    \          end\n\
+    \        }\n\
+    \      }\n\
+    \  in map"
+
+let%test "case type annot" =
+  test_parse
+    "\n\
+    \  let a =\n\
+    \    \\f.{\n\
+    \      case f\n\
+    \       | 1 : Int => 1\n\
+    \       | 2 => 2\n\
+    \      end\n\
+    \    }\n\
+    \  in a 1"
