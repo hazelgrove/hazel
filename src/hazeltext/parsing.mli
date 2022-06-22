26f645d5733f8df0282c3ea85e8844a5d416a4d2
Eric Zhao <21zhaoe@protonmail.com>
2022-06-22 16:29:00 -0400

    Add back Parsing.ast_of_lexbuf

--- src/hazeltext/parsing.mli
+++ src/hazeltext/parsing.mli
@@ -1,3 +1,7 @@
+(* Parses the given text lexbuf, returning a result of a UHExp.t,
+   or a string with the given error message *)
+val ast_of_lexbuf : Lexing.lexbuf -> (UHExp.t, string) result
+
 (* Parses the given text string, returning a result of a UHExp.t,
    or a string with the given error message *)
 val ast_of_string : string -> (UHExp.t, string) result
