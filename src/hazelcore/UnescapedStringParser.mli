cccceac8bbe9362655c61ef37911b0a7bb9c7f17
Eric Zhao <21zhaoe@protonmail.com>
2022-07-14 13:03:13 -0400

    Add .ocamlformat

--- src/hazelcore/UnescapedStringParser.mli
+++ src/hazelcore/UnescapedStringParser.mli
@@ -1,25 +1,18 @@
+type valid_seq = { start : int; ostart : int; length : int; olength : int }
+[@@deriving sexp]
 (** The type for a valid escape sequence. *)
-type valid_seq = {
-  start: int;
-  ostart: int;
-  length: int;
-  olength: int;
-} [@@deriving sexp]
 
+type invalid_seq = { start : int; ostart : int; length : int } [@@deriving sexp]
 (** The type for an invalid escape sequence. *)
-type invalid_seq = {
-  start: int;
-  ostart: int;
-  length: int;
-} [@@deriving sexp]
 
 type parsed = {
-  str: UnescapedString.t;
-  vseqs: valid_seq list;
-  iseqs: invalid_seq list;
-} [@@deriving sexp]
+  str : UnescapedString.t;
+  vseqs : valid_seq list;
+  iseqs : invalid_seq list;
+}
+[@@deriving sexp]
 
+val parse : Lexing.lexbuf -> parsed
 (** Parse a string literal. *)
-val parse: Lexing.lexbuf -> parsed
 
-val from_string: string -> parsed
+val from_string : string -> parsed
