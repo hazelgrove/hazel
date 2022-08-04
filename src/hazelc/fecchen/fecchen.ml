efdab0f0037ab35447319c215b7085ae8c04ef46
Eric Zhao <21zhaoe@protonmail.com>
2022-08-04 15:54:06 -0400

    fecchen: Clean up fetch,fetch_with_memo

--- src/hazelc/fecchen/fecchen.ml
+++ src/hazelc/fecchen/fecchen.ml
@@ -21,17 +21,15 @@ module Make (Query : Query.T) : S with type 'd query = 'd Query.t = struct
 
   module Monad = Monad.Make (Query)
   module Syntax = Monad.Syntax
+  open Monad
+  open Monad.Syntax
 
   let store_memo q d =
-    let open Monad in
-    update (fun { memo } ->
+    update (fun ({ memo; _ } as s) ->
         Memo.store memo q d;
-        { memo })
+        { s with memo })
 
-  let retrieve_memo q =
-    let open Monad in
-    let open Monad.Syntax in
-    get >>| fun { memo } -> Memo.retrieve memo q
+  let retrieve_memo q = get >>| fun { memo; _ } -> Memo.retrieve memo q
 
   type 'd f = {
     fetch : 'd Query.t -> ('d, 'd) Monad.t;
@@ -41,45 +39,37 @@ module Make (Query : Query.T) : S with type 'd query = 'd Query.t = struct
 
   type 'd rules = 'd f -> 'd Query.t -> ('d, 'd) Monad.t
 
-  let run rules q =
-    let open Monad in
-    let rec fetch rules q =
-      let fetch' q' = fetch rules q' in
-      let pure d = d |> return in
-      let io d = d |> return in
+  let rec fetch rules q =
+    let pure d = d |> return in
+    let io d = d |> return in
 
-      (* Invoke rules. *)
-      rules { fetch = fetch'; pure; io } q
+    let fetch_ q = rules { fetch = fetch rules; pure; io } q in
+    fetch_ q
+
+  let rec fetch_with_memo rules q =
+    let pure d =
+      let+ () = store_memo q d in
+      d
     in
 
+    let io d =
+      let+ () = store_memo q d in
+      d
+    in
+
+    let fetch_with_memo_ q =
+      retrieve_memo q >>= function
+      | Some d -> d |> return
+      | None -> rules { fetch = fetch_with_memo rules; pure; io } q
+    in
+
+    fetch_with_memo_ q
+
+  let run rules q =
     let m = fetch rules q in
     run_state m (init ())
 
   let run_with_memo rules q =
-    let open Monad in
-    let open Monad.Syntax in
-    let rec fetch rules q =
-      let fetch' q' =
-        retrieve_memo q' >>= function
-        | Some d -> d |> return
-        | None -> fetch rules q'
-      in
-
-      let pure d =
-        let+ () = store_memo q d in
-        d
-      in
-
-      let io d =
-        let+ () = store_memo q d in
-        d
-      in
-
-      (* Invoke rules. *)
-      rules { fetch = fetch'; pure; io } q
-    in
-
-    let open Monad in
-    let m = fetch rules q in
+    let m = fetch_with_memo rules q in
     run_state m (init ())
 end
