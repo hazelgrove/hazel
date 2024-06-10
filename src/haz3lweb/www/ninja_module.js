e45bda7c5c43c232b265af99d757544fa18dab11
Alexander Bandukwala <7h3kk1d@gmail.com>
2024-06-10 08:41:34 -0500

    Add another comment describing the ninja-keys clipboard-shim override

--- src/haz3lweb/www/ninja_module.js
+++ src/haz3lweb/www/ninja_module.js
@@ -1,11 +1,17 @@
 import {NinjaKeys} from 'https://unpkg.com/ninja-keys?module';
 import hotkeys from "https://unpkg.com/hotkeys-js@3.8.7?module"
+
+
+// This is the default behavior for the hotkeys module but I'm overriding it for the clipboard-shim
 hotkeys.filter = event => {
   const target = event.target || event.srcElement;
   const { tagName, id } = target;
+
+  // Override happening here
   if(id == "clipboard-shim") {
     return true;
   }
+  
   let flag = true;
   const isInput = tagName === 'INPUT' && !['checkbox', 'radio', 'range', 'button', 'file', 'reset', 'submit', 'color'].includes(target.type);
   // ignore: isContentEditable === 'true', <input> and <textarea> when readOnly state is false, <select>
