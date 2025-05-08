0c0c18e0b23c8a9b41a9e2fe6b89620296d6107f
Gregory Croisdale <gregtc@umich.edu>
2025-05-08 12:27:39 -0400

    added init pieces

---
+++ src/haz3lweb/init/docs/Conditionals.ml
@@ -0,0 +1,9 @@
+let out : Haz3lcore.PersistentZipper.t =
+  {
+    zipper =
+      "((selection((focus Left)(content())(mode \
+       Normal)))(backpack())(relatives((siblings(()((Grout((id \
+       5eb4e750-954c-46e7-a785-d8af9e1b8e40)(shape \
+       Convex))))))(ancestors())))(caret Outer))";
+    backup_text = " ";
+  }
