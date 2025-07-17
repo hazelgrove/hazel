f6b94bfde2d6df2bcf858cd101a9dc78f0945a46
Alexander Bandukwala <7h3kk1d@gmail.com>
2025-07-17 17:36:49 -0400

    Added more of the B2T2 Table API documentation slides

---
+++ src/web/init/docs/B2T2TableAPISubtable.ml
@@ -0,0 +1,10 @@
+let out : string * Haz3lcore.PersistentZipper.t =
+  ( "[B2T2] Table API Subtable",
+    {
+      zipper =
+        "((selection((focus Left)(content())(mode \
+         Normal)))(backpack())(relatives((siblings(()((Grout((id \
+         82323b14-b503-4be3-87e6-eb80ac0db4bd)(shape \
+         Convex))))))(ancestors())))(caret Outer))";
+      backup_text = "";
+    } )
