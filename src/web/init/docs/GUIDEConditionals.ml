9abc2b476d6ba5ccd0d14b41e9a24b0a190d3dbe
Alexander Bandukwala <alexander@bandukwala.me>
2025-07-31 11:15:27 -0400

    Fix conditionals buffer

--- src/web/init/docs/GUIDEConditionals.ml
+++ src/web/init/docs/GUIDEConditionals.ml
@@ -3,8 +3,8 @@ let out : string * Haz3lcore.PersistentZipper.t =
     {
       zipper =
         "((selection((focus Left)(content())(mode \
-         Normal)))(relatives((siblings(((Grout((id \
-         01fdbb7f-e585-4010-b24e-0e1130f8e74f)(shape \
-         Convex))))()))(ancestors())))(caret Outer))";
+         Normal)))(relatives((siblings(()((Grout((id \
+         5eb4e750-954c-46e7-a785-d8af9e1b8e40)(shape \
+         Convex))))))(ancestors())))(caret Outer))";
       backup_text = "";
     } )
